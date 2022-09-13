e4c22edbebdef6f123d1d64a099a348f0beaff04
RoberOrz <contacto@iconikalstudio.com>
2022-09-13 03:55:48 -0500

    alpha.0.0.0.1

---
+++ components/LayoutWrapper.js
@@ -0,0 +1,21 @@
+import siteMetadata from '@/data/siteMetadata'
+import headerNavLinks from '@/data/headerNavLinks'
+import Logo from '@/data/logo.svg'
+import Link from './Link'
+import SectionContainer from './SectionContainer'
+import Footer from './Footer'
+import MobileNav from './MobileNav'
+import ThemeSwitch from './ThemeSwitch'
+
+const LayoutWrapper = ({ children }) => {
+  return (
+    <SectionContainer>
+      <div className="flex h-screen flex-col justify-between">
+        <main className="mb-auto">{children}</main>
+        <Footer />
+      </div>
+    </SectionContainer>
+  )
+}
+
+export default LayoutWrapper
